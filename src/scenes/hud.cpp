ad698f9a8568ba1048ea010785ec2f2f558a5770
Fredrik <fredrik.k.andersson@gmail.com>
2019-04-13 18:37:42 +0200

    Center UI controls on all screen resolutions

--- src/scenes/hud.cpp
+++ src/scenes/hud.cpp
@@ -18,8 +18,8 @@ namespace godot {
     
   }
   void HUD::_ready() {
-    get_node("StartButton")->connect("pressed", this, "onStartNewGamePressed");
-    get_node("MessageTimer")->connect("timeout", this, "onMessageTimerTimeout");
+    find_node("StartButton")->connect("pressed", this, "onStartNewGamePressed");
+    find_node("MessageTimer")->connect("timeout", this, "onMessageTimerTimeout");
     
     // The "Player"  node is not a child of this node, 
     // so let's search for it globally
@@ -27,33 +27,33 @@ namespace godot {
   }
 
   void HUD::onStartNewGamePressed() {
-    cast_to<Button>(get_node("StartButton"))->hide();
+    cast_to<Button>(find_node("StartButton"))->hide();
     showMessage("Get ready!");
     emit_signal("startGame");
   }
 
   void HUD::onGameOver() {
     showMessage("Game over", true);
-    cast_to<Button>(get_node("StartButton"))->show();
+    cast_to<Button>(find_node("StartButton"))->show();
   }
 
   void HUD::onMessageTimerTimeout() {
-    cast_to<Label>(get_node("MessageLabel"))->hide();
+    cast_to<Label>(find_node("MessageLabel"))->hide();
   }
 
   void HUD::showMessage(godot::String message, bool timeout) {
-    auto msgLabel = cast_to<Label>(get_node("MessageLabel"));
+    auto msgLabel = cast_to<Label>(find_node("MessageLabel"));
     msgLabel->set_text(message);
     msgLabel->show();
 
     if(timeout == true) {
-      auto timer = cast_to<Timer>(get_node("MessageTimer"));
+      auto timer = cast_to<Timer>(find_node("MessageTimer"));
       timer->start();
     }
   }
 
   void HUD::showScore(const int score) {
-    auto scoreLabel =  cast_to<Label>(get_node("ScoreLabel"));
+    auto scoreLabel =  cast_to<Label>(find_node("ScoreLabel"));
     auto s = godot::String("{0}");
     scoreLabel->set_text(s.format(godot::Array::make(score)));
   }
