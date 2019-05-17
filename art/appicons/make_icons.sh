948528d29088e9ec2aaa1b6d23d5183a263551ef
Fredrik <fredrik.k.andersson@gmail.com>
2019-05-17 21:15:19 +0200

    First commit

---
+++ art/appicons/make_icons.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sips -Z 20 playerGrey_up1.1024.png --out icon-80.png
+sips -Z 40 playerGrey_up1.1024.png --out icon-40.png
+sips -Z 29 playerGrey_up1.1024.png --out icon-29.png
+sips -Z 60 playerGrey_up1.1024.png --out icon-60.png
+sips -Z 58 playerGrey_up1.1024.png --out icon-58.png
+sips -Z 87 playerGrey_up1.1024.png --out icon-87.png
+sips -Z 80 playerGrey_up1.1024.png --out icon-80.png
+sips -Z 120 playerGrey_up1.1024.png --out icon-120.png
+sips -Z 57 playerGrey_up1.1024.png --out icon-57.png
+sips -Z 114 playerGrey_up1.1024.png --out icon-114.png
+sips -Z 120 playerGrey_up1.1024.png --out icon-120.png
+sips -Z 180 playerGrey_up1.1024.png --out icon-180.png
+sips -Z 50 playerGrey_up1.1024.png --out icon-50.png
+sips -Z 100 playerGrey_up1.1024.png --out icon-.100.png
+sips -Z 72 playerGrey_up1.1024.png --out icon-72.png
+sips -Z 144 playerGrey_up1.1024.png --out icon-144.png
+sips -Z 76 playerGrey_up1.1024.png --out icon-76.png
+sips -Z 152 playerGrey_up1.1024.png --out icon-152.png
+sips -Z 167 playerGrey_up1.1024.png --out icon-167.png
+sips -Z 1024 playerGrey_up1.1024.png --out icon-1024.png
